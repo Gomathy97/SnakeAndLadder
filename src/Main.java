39dbd77e02a517a4c6043395f08db4f8f0653b58
gonagarajan <gonagarajan@paypal.com>
2024-04-08 20:29:21 +0530

    Snake and Ladder

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import service.BoardService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BoardService service = new BoardService();
+        service.initializeAndPlay();
+
+    }
+}
